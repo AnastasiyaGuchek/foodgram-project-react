ebcfce14360bbe4de1f544f568ef357a9e18c8e3
Анастасия Гучек <anastasia.guchek@mail.ru>
2023-05-31 10:35:55 +0300

    первая попытка

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -75,15 +75,15 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 """
+
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
@@ -94,7 +94,7 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT')
     }
 }
-"""
+
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
