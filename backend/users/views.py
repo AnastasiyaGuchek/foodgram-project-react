eef6479b31838eaabc5860097add96926d984b07
Анастасия Гучек <anastasia.guchek@mail.ru>
2023-06-15 22:06:08 +0300

    code according to PEP8

--- backend/users/views.py
+++ backend/users/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .models import Subscribe
@@ -42,6 +42,7 @@ class CustomUserViewSet(UserViewSet):
                                              author=author)
             subscription.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
+        
     @action(
         detail=False,
         permission_classes=[IsAuthenticated]
